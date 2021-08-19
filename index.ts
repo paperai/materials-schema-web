a2127c99a64b31551c55cef73f395bb7cee998e6
cindy <cindy@lookingup.co.jp>
2021-08-19 09:13:20 +0000

    Make html created by parsed types

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import * as fs from "fs";
-// import path from "path";
-// path.join(__dirname, "public")
+import path from "path";
+// 
 
 import { MaterialParser } from "./src/parse/MaterialParser";
 import { Page } from "./src/htmlWrite/Page";
@@ -9,9 +9,14 @@ import { TypeContent } from "./src/htmlWrite/TypeContent";
 if (typeof require !== "undefined" && require.main === module) {
 	console.log("Program Start...");
 	const parser = new MaterialParser();
-	const typer = new TypeContent("T100", parser.schema);
-	const pager = new Page("T100", typer.writeTable());
-	fs.writeFileSync("out.html", pager.htmlStr, { encoding: "utf-8" });
+
+	for (const [typeId, type] of Object.entries(parser.schema.types)) {
+		const typeContent = new TypeContent(type, parser.schema);
+		const pager = new Page(typeId, typeContent.writeTable());
+		fs.writeFileSync(path.join(__dirname, "src", "app", "views", "types", `${type.name}.html`), pager.htmlStr, {
+			encoding: "utf-8",
+		});
+	}
 	// console.log(pager.htmlStr);
 	// console.log("dataTypes = " + JSON.stringify(parser.schema.dataTypes, undefined, 4));
 	// console.log("types = " + JSON.stringify(parser.schema.types, undefined, 4));
