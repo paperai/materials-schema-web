48bf2f2cc73ec01201642c88e6634ee5e04663e3
cindy <cindy@lookingup.co.jp>
2021-08-19 09:14:17 +0000

    Add property router.

--- src/schema/Property.ts
+++ src/schema/Property.ts
@@ -1,10 +1,25 @@
+import { DataType } from "./DataType";
+import { Type } from "./Type";
+
 export class Property {
 	constructor(
 		readonly id: string,
 		readonly name: string,
-		readonly expectedTypes?: string[],
-		readonly description?: string
+		readonly expectedTypes: string[] = [],
+		readonly description = ""
 	) {
 		console.log("in Property");
+	}
+	public makeExpectedTypesName(recordedTypes: Record<string, Type>, recordedDataTypes: Record<string, DataType>) {
+		const expectedTypesName: string[] = [];
+		for (const typeIdOrDataTypeId of this.expectedTypes) {
+			if (typeof recordedTypes[typeIdOrDataTypeId] !== "undefined") {
+				expectedTypesName.push(recordedTypes[typeIdOrDataTypeId].name);
+			}
+			if (typeof recordedDataTypes[typeIdOrDataTypeId] !== "undefined") {
+				expectedTypesName.push(recordedDataTypes[typeIdOrDataTypeId].name);
+			}
+		}
+		return expectedTypesName;
 	}
 }
