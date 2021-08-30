ad5704196d03a113d832042019cba21df82b3ad6
cindy <cindy@lookingup.co.jp>
2021-08-30 10:37:41 +0000

    Document classes in schema.

--- src/schema/Property.ts
+++ src/schema/Property.ts
@@ -1,6 +1,9 @@
 import { DataType } from "./DataType";
 import { Type } from "./Type";
 
+/**
+ * Class Property.
+ */
 export class Property {
 	private _usedByTypesName: Set<string>;
 
@@ -8,16 +11,25 @@ export class Property {
 		return this._usedByTypesName;
 	}
 
+	/**
+	 * @param id - unique; recommend to starts with "P" for the ease of distinguishment
+	 * @param name - should be assigned in camelcase
+	 * @param expectedTypes - all `Type`s' `id` in which this `Property` can be found
+	 * @param description
+	 */
 	constructor(
 		readonly id: string,
 		readonly name: string,
 		readonly expectedTypes: string[] = [], // Type in Id format.
 		readonly description = ""
 	) {
-		console.log("in Property");
 		this._usedByTypesName = new Set();
 	}
 
+	/**
+	 * Making all `Type`s' `name` in which this `Property` can be found.
+	 * @note should make this function into class property if it will be called multiple times.
+	 */
 	public makeExpectedTypesName(recordedTypes: Record<string, Type> | Record<string, DataType>) {
 		const expectedTypesName: string[] = [];
 		for (const typeIdOrDataTypeId of this.expectedTypes) {
