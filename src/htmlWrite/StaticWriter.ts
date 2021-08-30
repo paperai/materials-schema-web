43f5ca4c02a9cd17f20e3d90b5df0b601755cbb6
cindy <cindy@lookingup.co.jp>
2021-08-30 10:44:58 +0000

    Comment out unneeded search icon. (might be added back later if search by keyup is slow)

--- src/htmlWrite/StaticWriter.ts
+++ src/htmlWrite/StaticWriter.ts
@@ -1,3 +1,8 @@
+/**
+ * A static functional html writer class.
+ *
+ * All functions that have no instance status and can be reused should be put into this class.
+ */
 export class StaticWriter {
 	constructor() {
 		throw new Error("DO NOT CALL THIS CLASS USING INSTANCE!");
@@ -26,16 +31,16 @@ export class StaticWriter {
 				})
 			})
 			</script>`;
-		const searchIcon =
-			`<svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="White" class="bi bi-search" viewBox="0 0 15 15">` +
-			`<path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>` +
-			`</svg>`;
+		// const searchIcon =
+		// 	`<svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="White" class="bi bi-search" viewBox="0 0 15 15">` +
+		// 	`<path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>` +
+		// 	`</svg>`;
 		let ret = "";
 		ret += `<div class="search-group my-5">`;
 		ret += `<div class="input-group">`;
 		ret += `<input id="searchInput" type="text" class="form-control" placeholder="Search by Name...">`;
 		ret += `<span class="input-group-btn" style="width: 45%;">`;
-		ret += `<button class="btn btn-primary" type="button">${searchIcon}</button>`;
+		// ret += `<button class="btn btn-primary" type="button">${searchIcon}</button>`;
 		ret += `</span>`;
 		ret += `</div>`;
 		ret += `</div>`;
